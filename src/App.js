a14a3082646da964c7e7e111c4bdfa492f7d352b
jdjake <denson@ualberta.ca>
2016-09-17 19:04:14 -0600

    awdiopawjdioawoidj

--- src/App.js
+++ src/App.js
@@ -90,10 +90,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="root">
-          <h1>Do You Want Pizza in {this.state.location}? </h1>
           <div className="container">
+            <h1>Do You Want Pizza in {this.state.location}? </h1>
             <QueryComponent Agent={this.Agent} onRecieveResponse={this.parseAgentResponse} />
             <PendingPizza pizza={this.state.pendingPizza} />
+            <img className="pizzaIMG center-block" src="pizza.svg"></img>
           </div>
       </div>
     );
